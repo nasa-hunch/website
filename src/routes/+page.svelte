2879edfde6ab737a51f7d054ba25a57d3794c8f0
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-10-20 21:33:48 -0400

    feat?: clip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,40 @@
 <header>
-	<h1>Make for <span class="nasa">NASA</span></h1>
-
+	<div class="text-container">
+		<h1>Make for NASA</h1>
+	</div>
 	<h2>
-		Apply real-life skills to real-life problems @ NASA, through the design and fabrication of valued products to be used in SPACE.
+		Apply real-life skills to real-life problems @ NASA, through the design and fabrication of valued products to be used in space.
 	</h2>
 </header>
 
 <style>
-	.nasa {
-		background: -webkit-linear-gradient(#105bd8, #0b3d91);
+	/* .nasa {
+		background: -webkit-linear-gradient(#02bfe7, #9bdaf1);
 		background-clip: text;
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
-		text-decoration: underline;
-		text-decoration-color: #0b3d91;
-		text-decoration-thickness: 0.5rem;
+	} */
+	.text-container {
+		background: linear-gradient(#02bfe7, #9bdaf1);
+		margin: 0 auto;
 	}
 
 	h1 {
-		font-size: 11rem;
+		font-size: 15rem;
+		color: white;
+		font-weight: 900;
 	}
 
 	h2 {
 		font-size: 2rem;
+		font-family: 'Lexend Variable', sans-serif;
+		color: white;
+		font-weight: 400;
 	}
 
 	header {
 		text-align: center;
+		background-color: #323a45;
+		padding: 5rem 0;
 	}
 </style>
