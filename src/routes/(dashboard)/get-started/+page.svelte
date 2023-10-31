5bf1da8fa40a366ece77169a2dde8dbe29c4c969
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2023-10-31 17:54:20 +0000

    Add input and button components and update register page

--- src/routes/(dashboard)/get-started/+page.svelte
+++ src/routes/(dashboard)/get-started/+page.svelte
@@ -1,32 +1,42 @@
 <script>
-    import ActiveHero from "$lib/ActiveHero";
-    $ActiveHero.disabled = true;
+	import Input from "$lib/components/Input.svelte";
+	import Button from "$lib/components/Button.svelte";
+	import Navbar from "$lib/Navbar.svelte";
 </script>
 
+
 <div class="wrap">
-	<form class="content">
-		<span class="inputDiv">
-			<input name="firstName" placeholder="First Name" />
-		</span>
-		<span class="inputDiv">
-			<input name="lastName" placeholder="Last Name" />
-		</span>
-		<span class="inputDiv">
-			<input name="email" type="email" placeholder="Email" />
-		</span>
-		<span class="inputDiv">
-			<input class="passwordHolder" name="pass1" placeholder="Password" type="password" />
-		</span>
-		<span class="inputDiv">
-			<input class="passwordHolder" name="pass2" placeholder="Confirm Password" type="password" />
-		</span>
-		<span class="inputDiv">
-			<button type="submit">Register</button>
-		</span>
-	</form>
+	<Navbar/>
+	<div class="contentWrap">
+		<form class="content">
+			<h1>Register</h1>
+			<span class="inputDiv">
+				<Input name="firstName" label="First Name"/>
+			</span>
+			<span class="inputDiv">
+				<Input name="lastName" label="last Name"/>
+			</span>
+			<span class="inputDiv">
+				<Input name="email" label="Email"/>
+			</span>
+			<span class="inputDiv">
+				<Input name="pass1" label="Password" customType="password"/>
+			</span>
+			<span class="inputDiv">
+				<Input name="pass2" label="Confirm Password" customType="password"/>
+			</span>
+			<span class="inputDiv">
+				<Button value="Register" type="submit"/>
+			</span>
+		</form>
+	</div>
+	
 </div>
 
 <style>
+	form {
+		background: white;
+	}
 	.wrap {
 		width: 100%;
 		height: 100vh;
@@ -35,15 +45,26 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: start;
+		background-image: url("cool_space.jpg");
+	}
+	.contentWrap {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background: rgba(255, 255, 255, 0.75);
+		backdrop-filter: blur(3px);
 	}
 	.content {
 		max-width: 350px;
 		width: 100%;
 		padding: 20px;
 		display: flex;
+		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		flex-direction: column;
+		border-radius: 10px;
 	}
 	input {
 		font-size: 1.2rem;
@@ -56,8 +77,9 @@
 		border: 1px solid gray;
 	}
 	.inputDiv {
-		margin: 5px;
+		margin: 7px;
 		width: 100%;
+		background: white;
 	}
 	input:hover {
 		border: 1px solid black;
