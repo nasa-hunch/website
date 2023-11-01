e94730f165e3cc2365e527eb77c02647e7b82545
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-11-01 19:21:55 -0400

    feat: hmr in docker compose

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		host: "0.0.0.0",
+		...((process.env.OUTER_PORT_FRONTEND && process.env.INNER_PORT_FRONTEND) ? {
+			hmr: {
+				clientPort: parseInt(process.env.OUTER_PORT_FRONTEND),
+			},
+			port: parseInt(process.env.INNER_PORT_FRONTEND), 
+		} : {}),
+		watch: {
+			usePolling: true
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
