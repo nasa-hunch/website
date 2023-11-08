b46319fbb76bc7b99b83fb3193182245c17060f0
Tristan F. <LeoDog896@hotmail.com>
2023-11-08 12:46:19 +0000

    feat: display something for admin/users

--- src/routes/dashboard/admin/users/+page.svelte
+++ src/routes/dashboard/admin/users/+page.svelte
@@ -1,4 +1,12 @@
-<script>
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+    export let data: PageData;
 </script>
 
 <h1>User Management Panel</h1>
+
+{#each data.userList as user}
+    <!-- TODO: proper user display -->
+    <p>{user.id}</p>
+{/each}
