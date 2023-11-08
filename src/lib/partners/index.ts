84bc0cf26f7cc9a3e96b58a8e9ae1f992f97a8f6
Tristan F. <LeoDog896@hotmail.com>
2023-11-08 13:36:09 +0000

    feat: partner list

---
+++ src/lib/partners/index.ts
@@ -0,0 +1,76 @@
+interface Partner {
+    name: string;
+    logo?: string;
+}
+
+const partners = [
+    {
+        name: "World Association of Chefs Societies",
+    },
+    {
+        name: "Lone Star Flight Museum"
+    },
+    {
+        name: "Houston Space Center"
+    },
+    {
+        name: "Edge Factor"
+    },
+    {
+        name: "Lockheed Martin"
+    },
+    {
+        name: "Sandvick Coromant"
+    },
+    {
+        name: "Haas Automation Inc."
+    },
+    {
+        name: "Sierra Nevada Corporation"
+    },
+    {
+        name: "techshot"
+    },
+    {
+        name: "Mastercam"
+    },
+    {
+        name: "Knights Manufacturing"
+    },
+    {
+        name: "Motorsports"
+    },
+    {
+        name: "Integrate Systems, Inc.",
+    },
+    {
+        name: "Space Foundation"
+    },
+    {
+        name: "GeneHaas Foundation"
+    },
+    {
+        name: "SME Education Foundation",
+    },
+    {
+        name: "Axiom Space"
+    },
+    {
+        name: "Blazing Trails"
+    },
+    {
+        name: "Association for Career and Technical Education"
+    },
+    {
+        name: "American Culinary Federation"
+    },
+    {
+        name: "Raspberry Pi"
+    },
+    {
+        name: "Eastern Florida State College"
+    },
+    {
+        name: "Sullivan University"
+    }
+]
