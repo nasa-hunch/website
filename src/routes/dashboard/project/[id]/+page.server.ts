4e62e129dde82a145e5ee9387bda6307b0174e56
Tristan F. <LeoDog896@hotmail.com>
2023-11-13 13:27:24 +0000

    feat: settings user information

--- src/routes/dashboard/project/[id]/+page.server.ts
+++ src/routes/dashboard/project/[id]/+page.server.ts
@@ -3,34 +3,38 @@ import type { PageServerLoad } from './$types';
 import { prisma } from '$lib/prismaConnection';
 import { Prisma, Role } from '@prisma/client';
 
-function filterProject(role: Role, orgId: number | null, id: number): Prisma.ProjectFindFirstArgs["where"] {
-    switch (role) {
-        case Role.HUNCH_ADMIN:
-            return {};
-        case Role.SCHOOL_ADMIN:
-            if (orgId == null) {
-                throw error(500, 'School admin has no orgId');
-            }
-            return {
-                organization: {
-                    id: orgId
-                }
-            };
-        case Role.TEACHER:
-            return {
-                owner: {
-                    id
-                }
-            };
-        default:
-            return {
-                users: {
-                    some: {
-                        userId: id
-                    }
-                }
-            };
-    }
+function filterProject(
+	role: Role,
+	orgId: number | null,
+	id: number
+): Prisma.ProjectFindFirstArgs['where'] {
+	switch (role) {
+		case Role.HUNCH_ADMIN:
+			return {};
+		case Role.SCHOOL_ADMIN:
+			if (orgId == null) {
+				throw error(500, 'School admin has no orgId');
+			}
+			return {
+				organization: {
+					id: orgId
+				}
+			};
+		case Role.TEACHER:
+			return {
+				owner: {
+					id
+				}
+			};
+		default:
+			return {
+				users: {
+					some: {
+						userId: id
+					}
+				}
+			};
+	}
 }
 
 export const load: PageServerLoad = async ({ params, parent }) => {
@@ -41,14 +45,14 @@ export const load: PageServerLoad = async ({ params, parent }) => {
 		where: {
 			id,
 			...filterProject(user.role, user.orgId, user.id)
-        }
+		}
 	});
 
-    if (!project) {
-        throw error(404, 'Project not found');
-    }
+	if (!project) {
+		throw error(404, 'Project not found');
+	}
 
-    return {
-        project
-    };
+	return {
+		project
+	};
 };
--- src/routes/dashboard/settings/+page.svelte
+++ src/routes/dashboard/settings/+page.svelte
@@ -1,5 +1,22 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: titleCasedRole =
+		data.user.role.toLowerCase().substring(0, 1).toUpperCase() +
+		data.user.role.toLowerCase().substring(1);
+</script>
+
 <main>
 	<h1>Settings</h1>
+
+	<h2>User Information</h2>
+
+	<p>First Name: {data.user.firstName}</p>
+	<p>Last Name: {data.user.lastName}</p>
+	<p>Email: <a href="mailto:{data.user.email}">{data.user.email}</a></p>
+	<p>Role: {titleCasedRole}</p>
 </main>
 
 <style>
