0e53ed7c89322c8953412de3a35d9db66a00b6ec
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2023-11-16 15:49:18 -0500

    Revert "Testing deployment __dirname fix"
    This reverts commit 16437971377c406ef32fc3e581b5287c4e8c7747.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@
 import type { PrismaClient as ImportedPrismaClient } from '@prisma/client';
 import { createRequire } from 'module';
 
-const require = createRequire(import.meta.url);
+const require = createRequire(import.meta.url ?? __filename);
 
 const { PrismaClient: RequiredPrismaClient } = require('@prisma/client');
 
