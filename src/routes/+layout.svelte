a39da02e8698f4bbcfa6840f6afb6ea5adb0dc6e
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-01-30 18:04:52 -0500

    feat: lenis

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,19 @@
 	import '@fontsource-variable/manrope';
 	import '@fontsource-variable/lexend';
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import Lenis from '@studio-freight/lenis'
+
+	onMount(() => {
+		const lenis = new Lenis()
+
+		function raf(time: number) {
+			lenis.raf(time)
+			requestAnimationFrame(raf)
+		}
+
+		requestAnimationFrame(raf)
+	})
 </script>
 
 <slot />
