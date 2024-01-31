03024369ffcc50621814c94e1410410122d505b7
Tristan F <LeoDog896@hotmail.com>
2024-01-31 09:44:12 -0500

    test: disable tests
    make auth tests later

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
-});
+// TODO: set up some auth tests
+// test('index page has expected h1', async ({ page }) => {
+// 	await page.goto('/');
+// 	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+// });
