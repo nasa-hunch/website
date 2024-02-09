6a6b75c1d01d7fb9af1f557985b3ca2ffcb00ef8
William Faircloth <willuhmjs@gmail.com>
2024-02-09 18:03:25 +0000

    feat: basic ul for connections

--- src/routes/(front)/connect/+page.svelte
+++ src/routes/(front)/connect/+page.svelte
@@ -4,4 +4,9 @@
 
 <Section title="Connect With Us">
 	<p>We have various social media accounts, and you can also contact us via email.</p>
+	<ul>
+		<li><a href="">Twitter</a></li>
+		<li><a href="">Facebook</a></li>
+		<li><a href="">YouTube</a></li>
+	</ul>
 </Section>
