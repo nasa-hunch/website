dbfbe3d8296bfd1d4ed8f870ed9ceb89245bb521
William Faircloth <willuhmjs@gmail.com>
2024-02-12 18:04:53 +0000

    feat: mobile optimized navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -47,85 +47,116 @@
         justify-content: center;
     }
 
-	.logoLink {
-		height: 100%;
-		width: 5rem;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		padding: 0px 3rem;
-		transition: opacity 0.2s cubic-bezier(0.075, 0.82, 0.165, 1);
-	}
+    .logoLink {
+        height: 100%;
+        width: 5rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0px 3rem;
+        transition: opacity 0.2s cubic-bezier(0.075, 0.82, 0.165, 1);
+    }
 
-	.navInner {
-		list-style-type: none;
-		width: 100%;
-		height: 100px;
-		display: flex;
-		padding: 0px;
-		margin: 0px;
-		flex-direction: row;
-		align-items: center;
-	}
-	.navInner li {
-		margin: 0px auto;
-		padding: 0px;
-		height: 100%;
-		width: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-	.navInner li a {
-		text-decoration: none;
-		margin: 0px;
-		font-size: 1.5rem;
-		width: 100%;
-		height: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
+    .navInner {
+        list-style-type: none;
+        width: 100%;
+        height: 100px;
+        display: flex;
+        padding: 0px;
+        margin: 0px;
+        flex-direction: row;
+        align-items: center;
+    }
 
-	.navRegularLink a {
-		color: #212121;
-		position: relative;
-	}
-	.navRegularLink a::after {
-		content: '';
-		position: absolute;
-		bottom: 0px;
-		left: 0px;
-		width: 100%;
-		height: 4px;
-		background: $primary;
-		transition: all 0.5s cubic-bezier(0.165, 0.84, 0.44, 1);
-		transform: scale(0, 1);
-	}
+    .navInner li {
+        margin: 0px auto;
+        padding: 0px;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
-	.navRegularLink a:hover {
-		color: $primary;
-		transition: all 0.5s cubic-bezier(0.165, 0.84, 0.44, 1);
-	}
-	.navRegularLink a:hover::after {
-		width: 100%;
-		transform: scale(1, 1);
-	}
+    .navInner li a {
+        text-decoration: none;
+        margin: 0px;
+        font-size: 1.5rem;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
-	.specialButton {
-		background-size: 300%;
-		background-position: 100%;
-		background-image: linear-gradient(to right, #99231b 40%, $primary 60%);
-		transition: all 0.2s ease-in-out;
-	}
-	.specialButton:hover {
-		background-position: 0%;
-	}
-	.specialButton a {
-		color: #f1f1f1;
-	}
+    .navRegularLink a {
+        color: #212121;
+        position: relative;
+    }
 
-	.logoLink:hover {
-		opacity: 0.6;
-	}
+    .navRegularLink a::after {
+        content: '';
+        position: absolute;
+        bottom: 0px;
+        left: 0px;
+        width: 100%;
+        height: 4px;
+        background: $primary;
+        transition: all 0.5s cubic-bezier(0.165, 0.84, 0.44, 1);
+        transform: scale(0, 1);
+    }
+
+    .navRegularLink a:hover {
+        color: $primary;
+        transition: all 0.5s cubic-bezier(0.165, 0.84, 0.44, 1);
+    }
+
+    .navRegularLink a:hover::after {
+        width: 100%;
+        transform: scale(1, 1);
+    }
+
+    .specialButton {
+        background-size: 300%;
+        background-position: 100%;
+        background-image: linear-gradient(to right, #99231b 40%, $primary 60%);
+        transition: all 0.2s ease-in-out;
+    }
+
+    .specialButton:hover {
+        background-position: 0%;
+    }
+
+    .specialButton a {
+        color: #f1f1f1;
+    }
+
+    .logoLink:hover {
+        opacity: 0.6;
+    }
+
+    @media (max-width: 768px) {
+        nav {
+            flex-direction: column;
+        }
+
+        .logoLink {
+            width: 100%;
+            padding: 1rem;
+        }
+
+        .navInner {
+            flex-direction: column;
+            height: auto;
+        }
+
+        .navInner li {
+            width: 100%;
+            padding: 1rem 0;
+        }
+
+        .navInner li a {
+            font-size: 1.2rem;
+        }
+    }
 </style>
