85462751a80b041550c6032a93c09986b883e795
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-02-20 05:32:44 -0500

    creating projects & project display

--- src/routes/dashboard/projects/+page.server.ts
+++ src/routes/dashboard/projects/+page.server.ts
@@ -1,14 +1,77 @@
+import { z } from "zod"
+
+import { formHandler } from "$lib/bodyguard.js"
+import { ProjectUserPermission, Role } from "$lib/enums.js"
 import { prisma } from "$lib/prismaConnection"
+import { verifySession } from "$lib/verifySession.js"
 
 export const load = async () => {
-	const categories = await prisma.category.findMany({})
 
 	const possibleProjects = await prisma.projectTemplate.findMany({})
 
 	console.log(possibleProjects)
 
 	return {
-		categories,
 		possibleProjects
 	}
+}
+
+export const actions = {
+	createProject: formHandler(z.object({
+		projectId: z.coerce.number()
+	}), async({projectId}, {cookies}) => {
+
+		const user = await verifySession(cookies);
+		if(user.role != Role.HUNCH_ADMIN && user.role != Role.SCHOOL_ADMIN && user.role != Role.TEACHER) {
+			return {
+				success: false,
+				message: "No Permissions"
+			}
+		}
+
+		if(!user.orgId) {
+			return {
+				success: false,
+				message: "No Organization."
+			}
+		}
+
+		const projectTemplate = await prisma.projectTemplate.findUnique({
+			where: {
+				id: projectId
+			}
+		})
+
+		if(!projectTemplate) {
+			return {
+				success: false,
+				message: "No Project Template"
+			}
+		}
+
+		const joinCode = 1e6 + Math.floor(Math.random() * 9e6)
+
+		await prisma.project.create({
+			data: {
+				name: projectTemplate.name,
+				description: projectTemplate.description,
+				orgId: user.orgId,
+				projectTemplateId: projectTemplate.id ,
+				joinCode: joinCode,
+				users: {
+					create: {
+						userId: user.id,
+						owner: true,
+						permission: ProjectUserPermission.EDITOR
+					}
+				}
+			}
+		})
+
+		return {
+			success: true,
+			message: "Project Created"
+		}
+
+	})
 }
--- src/routes/dashboard/projects/+page.svelte
+++ src/routes/dashboard/projects/+page.svelte
@@ -2,48 +2,47 @@
 	import ModelHelper from '$lib/components/ModelHelper.svelte';
 	import ModelForm from "$lib/components/ModelForm.svelte"
 	import TextButton from '$lib/components/TextButton.svelte';
+	import Button from "$lib/components/Button.svelte"
 	export let data;
 	import ComboBox from "$lib/components/Combobox.svelte"
+	import { Role } from '$lib/enums';
 	let creatingProject = false;
 
 	let selectedCategory: number;
-
-	let projectOptions = data.possibleProjects;
-	console.log(projectOptions)
-
-	$: if(selectedCategory) {
-		projectOptions = data.possibleProjects.filter((item) => item.categoryId == selectedCategory)
-	}
 	
 
 </script>
 
 <ModelHelper bind:visible={creatingProject}>
 	<ModelForm method="post" action="?/createProject">
-		<div>
-			<ComboBox options={[data.categories, (category) => category.id, (category) => category.name]} label="Project Category" bind:selected={selectedCategory}/>
-			<hr class="spacer">
-			{#if projectOptions && projectOptions.length > 0}
-				<ComboBox options={[projectOptions, (project) => project.id, (project) => project.name]} label="Project"/>
-			{/if}
-		</div>
+			<h2>Creating Project</h2>
+			<ComboBox options={[data.possibleProjects, (project) => project.id, (project) => project.name]} label="Project" name="projectId"/>
+			<hr class="spacer"/>
+			<Button value="Create"/>
 	</ModelForm>
 	
 </ModelHelper>
 
 <main>
-	{#each data.user.projectUser as projectUser}
-		<a class="project" href="/dashboard/projects/{projectUser.project.id}">
-			<p>{projectUser.project.name}</p>
-		</a>
-	{/each}
+	<h1>Projects</h1>
+	<div class="projects">
+		
+		{#each data.user.projectUser as projectUser}
+			<a class="project" href="/dashboard/projects/{projectUser.project.id}">
+				<h3>{projectUser.project.name}</h3>
+			</a>
+		{/each}
+	</div>
+	
 	<div class="createNewPrompt">
 		{#if data.user.projectUser.length < 1}
 			<p>Hmmm, looks like nothing is here yet. </p>
 		{:else}
 			<p>Not what you are looking for? </p>
 		{/if}
+		{#if data.user.role == Role.TEACHER || data.user.role == Role.SCHOOL_ADMIN}
 		<TextButton on:click={() => {creatingProject = true}}>Create a new project?</TextButton>
+		{/if}
 	</div>
 </main>
 
@@ -51,20 +50,43 @@
 	main {
 		width: 100%;
 		display: flex;
-		align-items: start;
-		justify-content: center;
+		flex-direction: column;
+		align-items: center;
+		justify-content: start;
 	}
 	.createNewPrompt {
 		display: flex;
 		align-items: center;
 		justify-content: center;
-
-		
-
-		
 	}
 	.spacer {
 		background: transparent;
 		border: 1px solid transparent;
+	}
+
+	.projects {
+		display: grid;
+		width: 90%;
+		gap: 10px;
+		grid-template-columns: repeat(auto-fill,minmax(300px,1fr));
+		
+	}
+
+	.project {
+		all: unset;
+		aspect-ratio: 3/2;
+		background: $background2;
+		border-radius: 10px;
+		padding: 25px;
+		box-sizing: border-box;
+		cursor: pointer;
+		
+		&:hover {
+			background: $background3;
+		}
+		h3 {
+			all: unset;
+			font-size: 1.25rem;
+		}
 	}
 </style>
