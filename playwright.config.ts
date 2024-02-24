4dd78d75dd78fecd63a145dd3cf73e20de4364c4
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-02-24 12:03:25 -0500

    ci: retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	retries: process.env.CI ? 2 : 0,
 	webServer: {
 		command: 'pnpm run build && pnpm exec dotenv node build',
 		port: 3000,
