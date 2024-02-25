3b48d82bc676b9d6fb156bd026c8a57bbe70f6f6
willuhmjs <willuhmjs@gmail.com>
2024-02-24 21:39:09 -0500

    feat: basic footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 	import { Toaster } from 'svelte-french-toast';
 	import { pwaInfo } from 'virtual:pwa-info';
 
+
 	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
 
@@ -19,11 +20,52 @@
 <div class="body">
 	<slot />
 </div>
-
+<footer>
+	<div>
+		<img src="/favicon.png" alt="NASA Hunch Logo" />
+	</div>
+	<div>
+		&copy; 2024 All Rights Reserved.
+	</div>
+	<div>
+		<div class="links">
+			<div class="link"><a href="/">T</a></div>
+			<div class="link"><a href="/">F</a></div>
+			<div class="link"><a href="/">Y</a></div>
+		</div>
+	</div>
+</footer>
 <style lang="scss">
-	:global(body) {
-		margin: 0px;
+    :global(body) {
+        margin: 0px;
+    }
 
-		--green: #158547;
-	}
+    :global(*) {
+        box-sizing: border-box;
+    }
+
+    footer {
+        padding: 1rem 3rem;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+
+		.links {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			justify-content: space-between;
+			width: 100%;
+
+			.link {
+				margin: 0px 0.5rem;
+			}
+		}
+    }
+
+    .copyright-container {
+        width: 100%;
+        text-align: center;
+    }
 </style>
