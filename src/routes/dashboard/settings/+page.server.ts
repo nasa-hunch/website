114df9a3107372a0039401960351d8ecd757d41f
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-02-25 09:48:15 -0500

    user settings page overhaul

---
+++ src/routes/dashboard/settings/+page.server.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+
+import { formHandler } from '$lib/bodyguard.js';
+import { prisma } from '$lib/prismaConnection.js';
+import { verifySession } from '$lib/verifySession.js';
+
+export const actions = {
+	updateAccountInfo: formHandler(z.object({
+		firstName: z.string().min(1),
+		lastName: z.string().min(1),
+		email: z.string().min(1)
+	}), async({firstName, lastName, email}, {cookies}) => {
+		const user = await verifySession(cookies)
+
+
+		await prisma.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				firstName,
+				lastName,
+				email
+			}
+		})
+
+		return {
+			success: true,
+			message: "User Updated!"
+		}
+
+	})
+}
--- src/routes/dashboard/settings/+page.svelte
+++ src/routes/dashboard/settings/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import Input from '$lib/components/Input.svelte';
+	import Button from '$lib/components/Button.svelte';
+import type { PageData } from './$types';
+	import toast from 'svelte-french-toast';
+	import { enhance } from '$app/forms';
 
 	export let data: PageData;
+	export let form;
+
+	$: if(form) {
+		if(form.success) {
+			toast.success(form.message || "Success!")
+		} else {
+			toast.error(form.message || "error.")
+		}
+	}
 
 	$: titleCasedRole =
 		data.user.role.toLowerCase().substring(0, 1).toUpperCase() +
@@ -9,18 +22,85 @@
 </script>
 
 <main>
-	<h1>Settings</h1>
+	<div class="wrap">
+		<h1>Settings</h1>
 
-	<h2>User Information</h2>
+		<form class="accountInfo" action="?/updateAccountInfo" method="post" use:enhance={() => {
+			return ({ update }) => {
+				update({ reset: false });
+			};
+		}}>
+			<div class="header">
+				<h2>Account Information</h2> <p class="role">{titleCasedRole}</p>
+			</div>
+			
 
-	<p>First Name: {data.user.firstName}</p>
-	<p>Last Name: {data.user.lastName}</p>
-	<p>Email: <a href="mailto:{data.user.email}">{data.user.email}</a></p>
-	<p>Role: {titleCasedRole}</p>
+			<Input value={data.user.firstName} label="First Name" name="firstName" bgColor="var(--background-alt)"/>
+			<hr>
+			<Input value={data.user.lastName} label="Last Name" name="lastName" bgColor="var(--background-alt)"/>
+			<hr>
+			<Input value={data.user.email} label="Email" name="email" bgColor="var(--background-alt)"/>
+			<hr>
+			<Button value="Save Information"/>
+			
+
+		</form>
+	
+
+		
+	</div>
+	
 </main>
 
 <style lang="scss">
-	main {
-		margin: 1rem;
+	.wrap {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.accountInfo {
+		width: 90%;
+		max-width: 600px;
+		background: $background-alt;
+		padding: 25px;
+		box-sizing: border-box;
+		border-radius: 10px;
+	}
+
+	hr {
+		border: transparent;
+	}
+	.header {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+
+		.role {
+			margin-left: 10px;
+			position: relative;
+			padding: 3px 5px;
+			z-index: 2;
+			border: 1px solid $secondary;
+			border-radius: 3px;
+
+			&::after {
+				content: "";
+				top: 0px;
+				left: 0px;
+				height: 100%;
+				width: 100%;
+				position: absolute;
+				background: $secondary;
+				z-index: -1;
+				opacity: 0.25;
+				border-radius: 3px;
+			}
+		}
+
+		
 	}
 </style>
