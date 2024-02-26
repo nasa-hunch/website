5e821ca0bf64301202e2740de7de43b48333a409
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-02-26 18:19:28 -0500

    overhaul dashboard

---
+++ src/routes/dashboard/+page.server.ts
@@ -0,0 +1,25 @@
+import { prisma } from '$lib/prismaConnection.js';
+
+export const load = async ({ parent }) => {
+	const parentData = await parent();
+
+	const assignees = await prisma.toDoAssignee.findMany({
+		where: {
+			AND: {
+				projectUser: {
+					userId: parentData.user.id
+				},
+				toDoItem: {
+					checked: false
+				}
+			}
+		},
+		include: {
+			toDoItem: true
+		}
+	});
+
+	return {
+		assignees
+	};
+};
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
-	import { Role } from '$lib/enums';
-
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
 <main>
-	<p>Welcome, {data.user.firstName} {data.user.lastName}!</p>
-	{#if data.user.role != Role.HUNCH_ADMIN}
-		<h1>Projects</h1>
-		{#each [...data.user.projectUser.map((user) => user.project)] as project}
-			<a class="project" href="/dashboard/projects/{project.id}">
-				<p>{project.name}</p>
-			</a>
-		{/each}
-	{/if}
+	<h2>Welcome back, {data.user.firstName} {data.user.lastName}!</h2>
+	<div class="wrap">
+		<div class="left">
+			<h1>Projects</h1>
+			<div class="projects">
+				{#each [...data.user.projectUser.map((user) => user.project)] as project}
+					<a class="project" href="/dashboard/projects/{project.id}">
+						<p>{project.name}</p>
+					</a>
+				{/each}
+			</div>
+		</div>
+		<div class="right">
+			<h2>To Do</h2>
+			<div class="items">
+				{#each data.assignees as assignee}
+					<a class="toDoItem" href="/dashboard/projects/{assignee.toDoItem.projectId}">
+						<h3>{assignee.toDoItem.name}</h3>
+					</a>
+				{/each}
+			</div>
+		</div>
+	</div>
 </main>
 
 <style lang="scss">
@@ -23,14 +35,31 @@
 		margin: 1rem;
 	}
 
+	.projects {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
+
 	.project {
-		margin: 1rem;
-		padding: 1rem;
-		border: 1px solid black;
-		border-radius: 0.5rem;
-		text-align: center;
-		background-color: #f0f0f0;
-		display: block;
+		all: unset;
+		height: 180px;
+		min-width: 300px;
+		background: $background-alt;
+		border-radius: 10px;
+		padding: 25px;
+		box-sizing: border-box;
+		cursor: pointer;
+		margin: 10px;
+		transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.25s;
+
+		&:hover {
+			background: $background2;
+		}
+		h3 {
+			all: unset;
+			font-size: 1.25rem;
+		}
 	}
 
 	.project:hover {
@@ -39,5 +68,53 @@
 
 	.project:active {
 		background-color: #d0d0d0;
+	}
+
+	.wrap {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.left {
+		width: 100%;
+	}
+
+	.right {
+		width: 100%;
+		max-width: 500px;
+		padding: 10px;
+		box-sizing: border-box;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.items {
+		width: 100%;
+	}
+
+	.toDoItem {
+		all: unset;
+		display: block;
+		cursor: pointer;
+		width: 100%;
+		background: $background-alt;
+		padding: 15px;
+		border-radius: 5px;
+		box-sizing: border-box;
+		text-align: left;
+		transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.25s;
+
+		&:hover {
+			background: $background2;
+		}
+
+		h3 {
+			margin: 0px;
+		}
+	}
+
+	h1 {
+		text-align: center;
 	}
 </style>
