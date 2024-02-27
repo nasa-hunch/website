4d841457c6b7bd6f4e44bf28920f78ac78c09d50
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-02-26 19:59:10 -0500

    update pfp component

--- src/lib/components/Pfp.svelte
+++ src/lib/components/Pfp.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let user: {
 		pfp: string | null;
 		firstName: string;
@@ -7,12 +9,35 @@
 
 	export let size = '24px';
 	export let marginRight = '10px';
+
+	let image: HTMLImageElement;
+
+	let imageSrc = user.pfp;
+
+	const tryPfp = async () => {
+		if (user.pfp) {
+			try {
+				const fetchTest = await fetch(new URL(user.pfp));
+
+				if (fetchTest.status != 200) {
+					imageSrc = '/defaultUser.svg';
+				}
+			} catch (e) {
+				imageSrc = '/defaultUser.svg';
+			}
+		} else {
+			imageSrc = '/defaultUser.svg';
+		}
+	};
+	onMount(() => {
+		tryPfp();
+	});
 </script>
 
 <img
-	style="width: {size}; height: {size}; margin-right: {marginRight}"
+	style="width: {size}; max-height: {size}; max-width: {size}; height: {size}; margin-right: {marginRight}"
 	alt="{user.firstName} {user.lastName}"
-	src={user.pfp || '/defaultUser.svg'}
+	src={imageSrc}
 />
 
 <style lang="scss">
