417496f9a1bae8d598ba080a9479f4639935d7b9
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-03-30 18:19:23 -0400

    fix: redirect to admin dashboard if admin

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,7 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+
 import { prisma } from '$lib/server/prisma/prismaConnection.js';
 
 export const load = async ({ parent }) => {
 	const parentData = await parent();
+
+	if (parentData.user.role === 'HUNCH_ADMIN') {
+		redirect(302, '/dashboard/admin');
+	}
 
 	const assignees = await prisma.toDoAssignee.findMany({
 		where: {
