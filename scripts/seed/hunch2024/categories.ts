5936eb9da81ff0eac8fe6a622efb22d4dff87a77
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-03-30 18:40:51 -0400

    chore: more seeding of software projects

--- scripts/seed/hunch2024/categories.ts
+++ scripts/seed/hunch2024/categories.ts
@@ -61,13 +61,43 @@ const categories: Omit<Prisma.CategoryCreateInput, 'icon' | 'deadline'>[] = [
 				},
 				{
 					name: `Creating a Next-Generation Website`,
-					description: `The objective of this project is to create a Next Generation
+					description: block(`The objective of this project is to create a Next Generation
                     WEB Site for NASA HUNCH that addresses the challenges faced by users while
                     accessing multiple websites for different purposes. The goal is to create a seamless
                     user experience by combining multiple existing websites into a single platform that
-                    offers personalized content, intuitive navigation, and visually appealing graphics.`,
+                    offers personalized content, intuitive navigation, and visually appealing graphics.`),
 					deadline: deadline()
-				}
+				},
+                {
+                    name: `Lunar, Mars, and Space Station Facility Environmental Control and Sensor Alarm System`,
+                    description: block(`Our Off-Plant Smart Building Software Controller provides
+                    a complete solution for managing and controlling the environment in a 2 - 3
+                    room smart building, off-planet. It features a comprehensive Control System,
+                    software, and sensors, compatible with Windows, Linux, PC, Arduino, and
+                    Rasberry Pie. This software provides an efficient and reliable way to monitor,
+                    adjust, and control the environment of space exploration.
+                    With our Off-Plant Smart Building Software Controller, you can rest assured
+                    that your space exploration is in safe and capable hands.`),
+                    deadline: deadline()
+                },
+                {
+                    name: `Defending the Smart Lunar Building: Cyber Security for Control Systems`,
+                    description: block(`As we embark on a new era of space exploration,
+                    the need for cyber security in smart lunar buildings, space stations,
+                    satellites, and other space-bound structures increases.`),
+                    deadline: deadline()
+                },
+                {
+                    name: `Personal Astronaut Assistant in Space`,
+                    description: block(`NASA HUNCH Software is on a mission to explore the boundaries
+                    of space and create an innovative system to help astronauts in their daily
+                    operations. To achieve this, we have developed a portable Personal Astronaut
+                    Assistant that is equipped with task support, health monitoring, companionship,
+                    lab assistant, and entertainment functions. By combining cutting-edge technology
+                    with our passion for space exploration, we’re creating a system that will revolutionize
+                    the way astronauts work and live in space.`),
+                    deadline: deadline()
+                }
 			]
 		}
 	},
