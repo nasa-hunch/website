3c2e850d76f011754ae2d07b65d8a8f49e8e01a4
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-04-01 17:08:01 -0400

    style: fmt
    Co-authored-by: William Faircloth <willuhmjs@gmail.com>

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,45 +1,45 @@
 <script lang="ts">
-  import { page } from '$app/stores';
-  import Navbar from '$lib/Navbar.svelte';
+	import { page } from '$app/stores';
+	import Navbar from '$lib/Navbar.svelte';
 </script>
 
 <Navbar />
 
 <div class="wrap">
-  <div class="content">
-    <h1 class="title">"Houston, we've had a problem here"</h1>
-    <h2><span class="errorCode">{$page.status}</span> {$page.error?.message}</h2>
-  </div>
+	<div class="content">
+		<h1 class="title">"Houston, we've had a problem here"</h1>
+		<h2><span class="errorCode">{$page.status}</span> {$page.error?.message}</h2>
+	</div>
 </div>
 
 <style lang="scss">
-  .errorCode {
-    color: $primary;
-  }
-  .content {
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    background: rgba(255, 255, 255, 0.75);
-    backdrop-filter: blur(3px);
-  }
-  .content h1 {
-    margin: 0px;
-    font-size: 2.5rem;
-  }
-  .content h2 {
-    font-size: 2rem;
-  }
+	.errorCode {
+		color: $primary;
+	}
+	.content {
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		background: rgba(255, 255, 255, 0.75);
+		backdrop-filter: blur(3px);
+	}
+	.content h1 {
+		margin: 0px;
+		font-size: 2.5rem;
+	}
+	.content h2 {
+		font-size: 2rem;
+	}
 
-  .wrap {
-    height: calc(100vh - 100px);
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    background-image: url('/cool_space2.jpg');
-    background-size: cover;
-    background-position: center;
-  }
+	.wrap {
+		height: calc(100vh - 100px);
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		background-image: url('/cool_space2.jpg');
+		background-size: cover;
+		background-position: center;
+	}
 </style>
