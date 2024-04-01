77199154599bc9cd736eea624438289584c96408
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-04-01 17:07:52 -0400

    style: fmt
    Co-authored-by: William Faircloth <willuhmjs@gmail.com>

--- src/lib/components/InTextInput.svelte
+++ src/lib/components/InTextInput.svelte
@@ -1,53 +1,53 @@
 <script lang="ts">
-  import { enhance } from '$app/forms';
+	import { enhance } from '$app/forms';
 
-  export let value: string;
-  export let action: string;
-  export let name: string = 'InTextInput';
-  export let disableFormMode = false;
+	export let value: string;
+	export let action: string;
+	export let name: string = 'InTextInput';
+	export let disableFormMode = false;
 
-  let buttonElement: HTMLButtonElement;
+	let buttonElement: HTMLButtonElement;
 
-  const submitForm = () => {
-    if (disableFormMode) {
-      return;
-    }
-    buttonElement.click();
-  };
+	const submitForm = () => {
+		if (disableFormMode) {
+			return;
+		}
+		buttonElement.click();
+	};
 </script>
 
 <form
-  class="wrap"
-  {action}
-  method="post"
-  on:submit
-  use:enhance={() => {
-    return ({ update }) => {
-      update({ reset: false });
-    };
-  }}
+	class="wrap"
+	{action}
+	method="post"
+	on:submit
+	use:enhance={() => {
+		return ({ update }) => {
+			update({ reset: false });
+		};
+	}}
 >
-  <input {name} class="inTextInput" on:change on:input on:change={submitForm} bind:value />
-  <div hidden>
-    <slot />
-  </div>
-  <button bind:this={buttonElement} hidden type="submit" />
+	<input {name} class="inTextInput" on:change on:input on:change={submitForm} bind:value />
+	<div hidden>
+		<slot />
+	</div>
+	<button bind:this={buttonElement} hidden type="submit" />
 </form>
 
 <style lang="scss">
-  .wrap {
-    width: 100%;
-    flex-grow: 1;
-  }
-  .inTextInput {
-    all: unset;
-    width: 100%;
-    padding: 0px 5px;
-    border-radius: 3px;
-    box-sizing: border-box;
-  }
-  .inTextInput:focus,
-  .inTextInput:active {
-    outline: 1px solid $primary;
-  }
+	.wrap {
+		width: 100%;
+		flex-grow: 1;
+	}
+	.inTextInput {
+		all: unset;
+		width: 100%;
+		padding: 0px 5px;
+		border-radius: 3px;
+		box-sizing: border-box;
+	}
+	.inTextInput:focus,
+	.inTextInput:active {
+		outline: 1px solid $primary;
+	}
 </style>
