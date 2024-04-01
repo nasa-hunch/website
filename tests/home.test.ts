5ca35fbaf2c906330ec532d48930daa7414b2a0e
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-04-01 17:08:09 -0400

    style: fmt
    Co-authored-by: William Faircloth <willuhmjs@gmail.com>

--- tests/home.test.ts
+++ tests/home.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('index page has expected title', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'NASA Hunch' })).toBeVisible();
+	await page.goto('/');
+	await expect(page.getByRole('heading', { name: 'NASA Hunch' })).toBeVisible();
 });
