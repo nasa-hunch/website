9f79f19e6a3d1caa18ef01433044c0ba4e01f9c7
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-04-03 10:48:28 -0400

    fix: drop admin view for projects
    unnecessary duplication. if you want this back, integrate it with projects

--- src/routes/dashboard/admin/projects/category/[catId]/[projectid]/files/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import FileManager from "$lib/fileManager/FileManager.svelte"
-	export let form;
-	export let data;
-	
-</script>
-
-<FileManager files={data.templateFiles} bind:form={form}/>
-
