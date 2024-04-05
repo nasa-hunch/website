f46be08bee041c70da3f105c532c6926dc6bdbc3
William Faircloth <willuhmjs@gmail.com>
2024-04-05 13:10:47 -0400

    feat: componetize user

---
+++ src/lib/components/User.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Pfp from '$lib/components/Pfp.svelte';
+
+	export let data;
+</script>
+
+<main>
+	<div class="title">
+		<Pfp user={data.selectedUser} size="100px" />
+		<h1>{data.selectedUser.firstName} {data.selectedUser.lastName}</h1>
+		<h2>{data.selectedUser.email}</h2>
+	</div>
+</main>
+
+<style lang="scss">
+	main {
+		margin-top: 1rem;
+	}
+
+	.title {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		padding: 1rem;
+		border-radius: 0.5rem;
+		background-color: var(--background-alt);
+		color: black;
+
+		img {
+			width: 100px;
+			height: 100px;
+			border-radius: 50%;
+		}
+	}
+</style>
--- src/routes/dashboard/users/[userId]/+page.svelte
+++ src/routes/dashboard/users/[userId]/+page.svelte
@@ -1,35 +1,6 @@
 <script lang="ts">
-	import Pfp from '$lib/components/Pfp.svelte';
+	import User from '$lib/components/User.svelte';
 
 	export let data;
 </script>
-
-<main>
-	<div class="title">
-		<Pfp user={data.user} size="100px" />
-		<h1>{data.selectedUser.firstName} {data.selectedUser.lastName}</h1>
-		<h2>{data.selectedUser.email}</h2>
-	</div>
-</main>
-
-<style lang="scss">
-	main {
-		margin-top: 1rem;
-	}
-
-	.title {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-		padding: 1rem;
-		border-radius: 0.5rem;
-		background-color: var(--background-alt);
-		color: black;
-
-		img {
-			width: 100px;
-			height: 100px;
-			border-radius: 50%;
-		}
-	}
-</style>
+<User {data} />
